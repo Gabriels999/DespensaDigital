6820f5d9cc738823f2b411523c9618c01ffdbb7e
Gabriel Silva Santos <gabriel.santos@buser.com.br>
2023-02-08 19:23:13 -0300

    show changes in svc

--- core/service/product_svc.py
+++ core/service/product_svc.py
@@ -16,13 +16,12 @@ def add_product(new_product):
     product.save()
     return product.to_dict_json()
 
-def edit_product(updated_product, id):
-
+def edit_product(new_version_product, id):
     product = Product.objects.get(id=id)
-    product.name = updated_product['name']
-    product.price = updated_product['price']
-    product.type = updated_product['type']
-    product.target_quantity = updated_product['target_quantity']
-    product.real_quantity = updated_product['real_quantity']
+    product.name = new_version_product['name']
+    product.price = new_version_product['price']
+    product.type = new_version_product['type']
+    product.target_quantity = new_version_product['target_quantity']
+    product.real_quantity = new_version_product['real_quantity']
     product.save()
     return product.to_dict_json()
