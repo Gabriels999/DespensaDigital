61f4755cdddadfe0c2a9eaaad09e92aea5a71fc6
Gabriel Silva Santos <gabriel.santos@buser.com.br>
2023-02-09 09:59:04 -0300

    uses the update from django on product_svc

--- core/service/product_svc.py
+++ core/service/product_svc.py
@@ -17,11 +17,12 @@ def add_product(new_product):
     return product.to_dict_json()
 
 def edit_product(new_version_product, id):
-    product = Product.objects.get(id=id)
-    product.name = new_version_product['name']
-    product.price = new_version_product['price']
-    product.type = new_version_product['type']
-    product.target_quantity = new_version_product['target_quantity']
-    product.real_quantity = new_version_product['real_quantity']
-    product.save()
-    return product.to_dict_json()
+    Product.objects.filter(id=id).update(
+        name=new_version_product['name'],
+        price=new_version_product['price'],
+        type=new_version_product['type'],
+        target_quantity=new_version_product['target_quantity'],
+        real_quantity=new_version_product['real_quantity'],
+    )
+    new_product = Product.objects.get(id=id)
+    return new_product.to_dict_json()
