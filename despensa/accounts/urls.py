c3795bb25475ffa9b4fcd41d1620084185856357
Gabriel Santos <gab.tec97@gmail.com>
2023-09-24 15:17:56 -0300

    working signup

--- despensa/accounts/urls.py
+++ despensa/accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('login', views.login),
+    path('signup', views.signup),
     path('logout', views.logout),
     path('whoami', views.whoami),
 ]
--- despensa/products/service/log_svc.py
+++ despensa/products/service/log_svc.py
@@ -1,11 +1,20 @@
-from ..models import ActivityLog
 import json
+
+from ..models import ActivityLog
 
 
 def log_login(user):
     logs = ActivityLog(
         type='login',
         logged_user=user,
+    )
+    logs.save()
+
+
+def log_signup(user):
+    logs = ActivityLog(
+        type='signup',
+        fromuser=user,
     )
     logs.save()
 
